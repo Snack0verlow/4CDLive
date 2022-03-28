825cc4f780abc00c120dd16b2535021cda3cc19c
BadgerBurrow <101304976+BadgerBurrow@users.noreply.github.com>
2022-03-28 15:11:50 -0700

    Added colors added homescreen

---
+++ app/src/main/java/com/snack0verflow/a4cdlive/HomeScreen.kt
@@ -0,0 +1,21 @@
+package com.snack0verflow.a4cdlive
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.snack0verflow.a4cdlive.ui.theme.HomeGreen
+
+@Composable
+fun HomeScreen () {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(HomeGreen)
+    ) {
+
+    }
+}
