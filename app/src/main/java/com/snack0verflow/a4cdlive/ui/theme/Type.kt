388bbe74a270c4aa377e493d073677f14f72ab4c
BadgerBurrow <101304976+BadgerBurrow@users.noreply.github.com>
2022-05-08 17:24:43 -0700

    Imported Font and R library to Type.kt for custom font

--- app/src/main/java/com/snack0verflow/a4cdlive/ui/theme/Type.kt
+++ app/src/main/java/com/snack0verflow/a4cdlive/ui/theme/Type.kt
@@ -3,20 +3,22 @@ package com.snack0verflow.a4cdlive.ui.theme
 import androidx.compose.material.Typography
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.snack0verflow.a4cdlive.R
 
 // TO DO:
 // - fix initialization of imported custom fonts
 // - change h2 fontfam -> fonts; fontweight -> ExtraBold
 // - change h3 fontfam -> fonts; fontweight -> Medium
-/*
+
 val fonts = FontFamily(
     Font(R.font.jost_medium, FontWeight.Medium),
     Font(R.font.inter_extrabold, FontWeight.ExtraBold)
 )
-*/
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
